90b86edd38f486a0172134ad378f456b8b70d549
Eugen Trippel <1540990+eugengo@users.noreply.github.com>
2025-08-31 22:04:18 +0200

    crazy initial

--- boards/arm/k03h/board.cmake
+++ boards/arm/k03h/board.cmake
@@ -1,5 +1,6 @@
 # This file was generated from a template. Edit it to match your keyboard.
 # See https://docs.zephyrproject.org/3.5.0/hardware/porting/board_porting.html#flash-and-debug-support
 # for more information.
-
+set(BOARD_FLASH_RUNNER uf2)
+set(FLASH_SCRIPT ${ZEPHYR_BASE}/boards/common/uf2.py)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
